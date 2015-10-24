f3d7737326573d4c139611c7b3ec025e557c15c2
amylinebaccay <amylinebaccay@yahoo.com>
2015-10-24 10:51:43 +0800

    bubble sort added

---
+++ bubbleSort.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class bubbleSort{
+
+  public static void main(String args[]){
+
+     int n = 20,a,b,swap;
+     Scanner in = new Scanner(System.in);
+
+     System.out.println("Enter number to sort");
+     n = in.nextInt();
+      int array[] = new int[n];
+      System.out.println("You enter " + n + "integers");
+
+      for(a =0;a< n;a++)
+        array[a] = in.nextInt();
+
+      for(a =0;a<(n - 1); a++){
+        for(b =0;b<n-a-1;b++){
+            if(array[b] > array[b+1]){
+                swap =array[b];
+                array[b] =array[b+1];
+                array[b+1] = swap;
+            }
+        }
+      }
+
+      System.out.println("Sorted list of numbers");
+
+       for(a=0;a<n;a++)
+        System.out.println(array[a]);
+
+  }
+
+}
